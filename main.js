13b056841324cbecd56c632a82373693d4c564a7
Aahana11873 <76442757+Aahana11873@users.noreply.github.com>
2021-12-28 21:43:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+ function setup(){
+        canvas=createCanvas(480, 380);
+        canvas.center();
+        video = createCapture(VIDEO);
+        video.hide();
+    } 
+    function draw() {
+        image(video, 0, 0, 600, 500);
+    }
